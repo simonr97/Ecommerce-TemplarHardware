874eeeec828e509f22b80cb2223c1d7c29e7b160
simonr97 <simon.rodriguez.1997@gmail.com>
2022-12-22 23:19:45 -0300

    PreEntrega1Rodriguez

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,10 @@
+import { ReactNode } from 'react';
+import { BiCart } from "react-icons/bi";
+
+const CartWidget = () => {
+    return (
+            <BiCart size="32px"/>
+    )
+}
+
+export default CartWidget;
